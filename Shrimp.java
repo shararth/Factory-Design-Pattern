fca9e8362dd231b9d94d9aa94653b821d786f810
shararth <78370741+shararth@users.noreply.github.com>
2021-02-01 22:15:53 -0600

    Add files via upload

---
+++ Shrimp.java
@@ -0,0 +1,11 @@
+
+public class ShrimpPizza  extends Pizza {
+	public ShirmpPizza() {
+		name = "Shrimp Pizza";
+		dough = "Thin crust";
+		sauce = "White garlic sauce";
+		toppings.add("Shrimp");
+		toppings.add("Grated parmesan cheese");
+		toppings.add("Olives");
+	}
+}
